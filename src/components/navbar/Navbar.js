a5b9b8c0416fab356053f4bd6f4f8710aaa06733
Agustina Gomez Fernandez <agustinagomez385@gmail.com>
2021-01-15 18:40:24 -0300

    navbar Mobile

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,42 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 const NavBar = ({ isScrolling }) => {
+	const [click, setClick] = useState(false);
+
+	console.log(click);
 	return (
 		<div>
 			<div className="submenu">
 				<nav className={`navbar ${isScrolling > 20 ? "scrolling" : null}`}>
-					<input type="checkbox" id="barra" className="barra"></input>
-					<label for="barra" className="hamburguesa">
+					<label
+						id="iconoMenu"
+						className={`hamburguesa ${click === true ? "visible" : null}`}
+						onClick={() => setClick(true)}
+					>
 						<i class="fas fa-bars"></i>
 					</label>
-					<label for="barra" className="close">
+					<label
+						id="iconoClose"
+						className={`close ${click === true ? "visible" : null}`}
+						onClick={() => setClick(false)}
+					>
 						<i class="fas fa-times"></i>
 					</label>
-					{/* Esto agrego */}
-					<ul>
+					<ul className="lista">
 						<li>
-							<a href="#sobreMi" className="link">
-								Sobre Mí{" "}
-							</a>{" "}
-						</li>{" "}
+							<a
+								href="#sobreMi"
+								className={`link ${click === true ? "visible" : "close"}`}
+								onClick={() => setClick(false)}
+							>
+								Sobre Mí
+							</a>
+						</li>
 						<li>
-							<a className="link" href="#habilidades">
-								Habilidades{" "}
-							</a>{" "}
-						</li>{" "}
+							<a
+								className="link"
+								href="#habilidades"
+								className={`link ${click === true ? "visible" : "close"}`}
+								onClick={() => setClick(false)}
+							>
+								Habilidades
+							</a>
+						</li>
 						<li>
-							<a className="link" href="#proyectos">
-								Proyectos{" "}
-							</a>{" "}
-						</li>{" "}
+							<a
+								className="link"
+								href="#proyectos"
+								className={`link ${click === true ? "visible" : "close"}`}
+								onClick={() => setClick(false)}
+							>
+								Proyectos
+							</a>
+						</li>
 						<li>
-							<a className="link" href="#contacto">
-								Contacto{" "}
-							</a>{" "}
-						</li>{" "}
-					</ul>{" "}
-				</nav>{" "}
+							<a
+								className="link"
+								href="#contacto"
+								className={`link ${click === true ? "visible" : "close"}`}
+								onClick={() => setClick(false)}
+							>
+								Contacto
+							</a>
+						</li>
+					</ul>
+				</nav>
 			</div>
 		</div>
 	);
