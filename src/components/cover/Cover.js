dc77fee60bfcfdd3683758a92e0b71e56b29f975
Agustina Gomez Fernandez <agustinagomez385@gmail.com>
2021-01-22 18:09:44 -0300

    last details

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Cover.css";
 import coverVideo from "../../media/video.mp4";
+import coverImage from "../../media/imagen.png";
+
 const Cover = () => {
+	const [isVideoLoaded, setIsVideoLoaded] = React.useState(false);
+	const onLoadedData = () => {
+		setIsVideoLoaded(true);
+	};
 	return (
 		<div className="cover-container">
-			<video className="video" src={coverVideo} autoPlay loop muted />
+			<img
+				src={coverImage}
+				className={`loading ${isVideoLoaded === true ? "load" : null}`}
+				alt="thumb"
+			/>
+			<video
+				className="video"
+				src={coverVideo}
+				width="100%"
+				height="100vh"
+				autoPlay
+				loop
+				muted
+				onLoadedData={onLoadedData}
+			/>{" "}
 			<h1> Agustina Gomez Fernandez </h1> <p> Developer Web Full Stack </p>{" "}
 		</div>
 	);
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,31 +2,33 @@ import React from "react";
 import "./Footer.css";
 const Footer = () => {
 	return (
-		<footer className="footer">
-			<div className="footer-contact">
-				<h3> Contacto </h3>{" "}
-			</div>{" "}
-			<div className="footer-sns">
-				<div className="sns-links">
-					<a
-						href="https://www.linkedin.com/in/agustina-gomez-fernandez/"
-						target="blank"
-					>
-						<i class="fab fa-linkedin linkedin"> </i>{" "}
-					</a>{" "}
-					<a href="https://twitter.com/Agustinagomezf" target="blank">
-						<i class="fab fa-twitter twitter"> </i>{" "}
-					</a>{" "}
-					<a href="https://github.com/AgustinaGF" target="blank">
-						<i class="fab fa-github github"> </i>{" "}
-					</a>{" "}
+		<div id="contacto">
+			<footer className="footer">
+				<div className="footer-contact">
+					<h3> Contacto </h3>{" "}
 				</div>{" "}
-			</div>{" "}
-			<div className="line"> </div>{" "}
-			<div className="desing-by">
-				Copyright© 2021 Agustina Gomez Fernandez | All Rights Reserved{" "}
-			</div>{" "}
-		</footer>
+				<div className="footer-sns">
+					<div className="sns-links">
+						<a
+							href="https://www.linkedin.com/in/agustina-gomez-fernandez/"
+							target="blank"
+						>
+							<i class="fab fa-linkedin linkedin"> </i>{" "}
+						</a>{" "}
+						<a href="https://twitter.com/Agustinagomezf" target="blank">
+							<i class="fab fa-twitter twitter"> </i>{" "}
+						</a>{" "}
+						<a href="https://github.com/AgustinaGF" target="blank">
+							<i class="fab fa-github github"> </i>{" "}
+						</a>{" "}
+					</div>{" "}
+				</div>{" "}
+				<div className="line"> </div>{" "}
+				<div className="desing-by">
+					Copyright© 2021 Agustina Gomez Fernandez | All Rights Reserved{" "}
+				</div>{" "}
+			</footer>{" "}
+		</div>
 	);
 };
 
